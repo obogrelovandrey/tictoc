4a2d841756e18b28a6c703daf299c16503a3e296
Andrey <nuft@ya.ru>
2017-08-21 01:13:06 +0700

    tictoc

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=tictoc
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/tictoc
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=tictoc.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/tictoc.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=tictoc
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/tictoc
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=tictoc.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/tictoc.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
